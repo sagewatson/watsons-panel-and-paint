45c50d8fb2ec8b179b76f8825fd4db684da009f0
Sage Watson <sagewatson@developersinstitute.co.nz>
2023-01-17 20:18:44 +1300

    Fixed production build

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <Header />
+        <title>Watson's Panel & Paint</title>
       </Head>
-
+      <Header />
       <main className={styles.main}>
         <MainSlider />
         <Services />
